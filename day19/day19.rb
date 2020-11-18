230072f140c77ee472c80b629571554d277228d5
Jonathan Lalande <lalandej.gg@gmail.com>
2020-11-18 18:58:10 -0500

    day19

---
+++ day19/day19.rb
@@ -0,0 +1,19 @@
+replacements = File.read('input')
+  .lines
+  .map { |line| line.chomp.split(' => ') }
+
+starting = "CRnCaCaCaSiRnBPTiMgArSiRnSiRnMgArSiRnCaFArTiTiBSiThFYCaFArCaCaSiThCaPBSiThSiThCaCaPTiRnPBSiThRnFArArCaCaSiThCaSiThSiRnMgArCaPTiBPRnFArSiThCaSiRnFArBCaSiRnCaPRnFArPMgYCaFArCaPTiTiTiBPBSiThCaPTiBPBSiRnFArBPBSiRnCaFArBPRnSiRnFArRnSiRnBFArCaFArCaCaCaSiThSiThCaCaPBPTiTiRnFArCaPTiBSiAlArPBCaCaCaCaCaSiRnMgArCaSiThFArThCaSiThCaSiRnCaFYCaSiRnFYFArFArCaSiRnFYFArCaSiRnBPMgArSiThPRnFArCaSiRnFArTiRnSiRnFYFArCaSiRnBFArCaSiRnTiMgArSiThCaSiThCaFArPRnFArSiRnFArTiTiTiTiBCaCaSiRnCaCaFYFArSiThCaPTiBPTiBCaSiThSiRnMgArCaF"
+molecules = []
+
+replacements.each do |from, to|
+  starting.chars.each.with_index do |_, i|
+    if starting[i..(i + from.size - 1)] == from
+      transformation = starting[..(i-1)] + to + starting[(i+from.size)..]
+      molecules << transformation
+    end
+  end
+end
+
+p molecules.uniq.size
+
+# yo fuck p2 lol
