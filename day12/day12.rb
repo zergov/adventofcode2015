8aada60b9bac82d07e2f1fa989b5dca07256cd11
Jonathan Lalande <lalandej.gg@gmail.com>
2020-11-28 19:11:54 -0500

    real day12

--- day12/day12.rb
+++ day12/day12.rb
@@ -1,1 +1,22 @@
-p File.read('input').scan(/-?\d*/).map(&:to_i).sum
+require 'json'
+
+def crawl(value)
+  if value.is_a?(Integer)
+    return value
+  end
+
+  if value.is_a?(Array)
+    return value.sum { |v| crawl(v) }
+  end
+
+  if value.is_a?(Hash)
+    values = value.values()
+    return 0 if value.key?("red") || values.include?("red")
+    return crawl(value.values())
+  end
+
+  return 0
+end
+
+json = JSON.parse(File.read('input').strip)
+p crawl(json)
